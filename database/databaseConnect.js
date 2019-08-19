f26e32b03745fcbd5b4d7acde3b2894d0793f5ce
Daniel Lee <ehd8266@naver.com>
2019-08-19 11:24:36 +0900

    Update databaseConnect.js

--- database/databaseConnect.js
+++ database/databaseConnect.js
@@ -1,8 +1,8 @@
 module.exports ={
   //Insert our WAS IP, DB ID, DB Password
-  host : '13.58.55.98', 
-  user : 'root',
-  password : 'Project3was!',
+  host : '', 
+  user : '',
+  password : '',
   port : 3306,
   database : 'mysql'
 }
