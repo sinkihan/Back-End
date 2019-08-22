7b8f3c19dd119edcfdd7efa468862409e78fe961
LDH <ehd8266@gmail.com>
2019-08-22 09:37:41 +0900

    DB에 게시글 추가하는 쿼리문 업데이트

--- routes/database.js
+++ routes/database.js
@@ -5,38 +5,40 @@ var mysql = require('mysql');
 var dbconfig = require('../database/databaseConnect');
 var connection = mysql.createConnection(dbconfig);
 
-router.get('/getTable', (req, res) => {
-    connection.query('SELECT * FROM client', (err, rows) => {
-        if (err) throw err;
+router.get('/getBoardList', (req, res) => {
+    connection.query('SELECT * FROM board', (err, result) => {
+        if (err) {
+            return err;
+        } else {
+            res.send(result);
+        }
+    })
+});
 
-        console.log('A result is : ', rows);
-        res.send(rows);
-    });
+router.get('/getBoardModify', (req, res) => {
+
 });
 
 router.post('/join', (req, res) => {
-
     connection.query(`SELECT COUNT(*) as count FROM client
     where user_email = '${req.body.email}' `, (err, result) => {
 
-            if (err) throw err
+            if (err) throw err;
             if (result[0].count === 1) {
 
-                res.send(false)
+                res.send(false);
             } else {
                 connection.query(`INSERT INTO client (user_email,user_firstName,user_lastName,user_password)
                 VALUES('${req.body.email}','${req.body.first}','${req.body.last}','${req.body.pw}')`, (err, result) => {
-                       if (err) throw err;
-                   })
-               
-               res.send(true)
+                        if (err) throw err;
+                    })
+
+                res.send(true);
             }
-        })
-})
+        });
+});
 
 router.post('/login', (req, res) => {
-    console.log(req.body);
-
     connection.query(`SELECT COUNT(*) as count FROM client
      where user_email = '${req.body.email}' and user_password = '${req.body.pw}'`, (err, result) => {
 
@@ -46,7 +48,25 @@ router.post('/login', (req, res) => {
             } else {
                 res.send(false);
             }
-        })
-})
+        });
+});
+
+router.post('/setBoard', (req, res) => {
+
+    connection.query(`INSERT INTO board (board_name, board_user, board_password, board_contents, board_date, board_title)
+    VALUES('${req.body.name}','${req.body.user}','${req.body.pw}','${req.body.contents}','${req.body.date}', '${req.body.title}')`,
+     (err, result) => {
+
+            if (err) {
+                res.send(false);
+            } else {
+                res.send(true);
+            }
+        });
+});
+
+router.post('/setBoardDelete', (req, res) => {
+
+});
 
 module.exports = router;
