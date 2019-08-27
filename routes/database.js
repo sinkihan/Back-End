856f1c9b38677a18c8e50ee68295ca1bb7df691c
LDH <ehd8266@gmail.com>
2019-08-27 17:01:05 +0900

    Update Back-End

--- routes/database.js
+++ routes/database.js
@@ -15,10 +15,6 @@ var connection = mysql.createConnection(dbconfig);
 //     })
 // });
 
-router.get('/getBoardModify', (req, res) => {
-
-});
-
 router.post('/join', (req, res) => {
     connection.query(`SELECT COUNT(*) as count FROM client
     where user_email = '${req.body.email}' `, (err, result) => {
@@ -39,15 +35,12 @@ router.post('/join', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
+    console.log(req.body);
+    
     connection.query(`SELECT COUNT(*) as count FROM client
      where user_email = '${req.body.email}' and user_password = '${req.body.pw}'`, (err, result) => {
-
-            if (err) throw err
-            if (result[0].count === 1) {
-                res.send(true);
-            } else {
-                res.send(false);
-            }
+            
+            (result[0].count === 1) ? res.send(true) : res.send(false);
         });
 });
 
@@ -55,18 +48,62 @@ router.post('/setBoard', (req, res) => {
 
     connection.query(`INSERT INTO board (board_name, board_user, board_password, board_contents, board_date, board_title)
     VALUES('${req.body.name}','${req.body.user}','${req.body.pw}','${req.body.contents}','${req.body.date}', '${req.body.title}')`,
-     (err, result) => {
+        (err, result) => {
 
-            if (err) {
-                res.send(false);
-            } else {
-                res.send(true);
-            }
+            (err) ? res.send(false) : res.send(true);
+        });
+});
+
+router.get('/getBoardContents', (req, res) => {
+
+    connection.query(`SELECT * FROM board where board_id = '${req.query.board_id}'`,
+        (err, result) => {
+
+            (err) ? res.send(err) : res.send(result);
         });
 });
 
 router.post('/setBoardDelete', (req, res) => {
 
+    connection.query(`SELECT COUNT(*) as count FROM board where board_id ='${req.body.board_id}' and
+    board_password = '${req.body.board_password}'`,
+        (err, result) => {
+
+            if (err) throw err;
+            if (result[0].count === 0) {
+
+                res.send(false);
+            } else {
+                connection.query(`DELETE FROM board where board_id = '${req.body.board_id}'`,
+                    (err, result) => {
+                        (err) ? res.send(err) : res.send(true);
+                    }
+                )
+            }
+        })
+});
+
+router.post('/getBoardModify', (req, res) => {
+    console.log(req.body);
+    
+    connection.query(`SELECT COUNT(*) as count FROM board where board_id ='${req.body.board_id}' and
+    board_password = '${req.body.board_password}'`,
+        (err, result) => {
+
+            if (err) throw err;
+            if (result[0].count === 0) {
+
+                res.send(false);
+            } else {
+                connection.query(`UPDATE board SET board_name = '${req.body.name}', board_user = '${req.body.user}', board_password = '${req.body.board_password}', board_contents = '${req.body.contents}', board_date = '${req.body.date}', board_title = '${req.body.title}' where board_id = ${req.body.board_id}`,
+                    (err, result) => {
+                        (err) ? res.send(err) : res.send(true);
+                        console.log(err);
+                        
+                    }
+                )
+            }
+        })
 });
 
 //---------------------------------------------------------
@@ -74,22 +111,20 @@ router.post('/setBoardDelete', (req, res) => {
 router.get('/getBoardCount', (req, res) => {
     connection.query('SELECT count(*) as count FROM board', (err, result) => {
         let count = result[0].count.toString();
-        
+
         (count !== 0) ? res.send(count) : res.send(0);
     })
- });
+});
 
- //get boardList by rownum indexing
- router.get('/getBoardList', (req, res) => {
-    connection.query('select @rownum:=@rownum+1 as rownum, board_id, board_title, board_name, board_date from board, (select @rownum:=0) TMP order by rownum desc', (err, result) => {
+//get boardList by rownum indexing
+router.get('/getBoardList/:currentPage', (req, res) => {
+    //  console.log(req.params);
 
-        if (err) {
-            return err;
-        } else {
-            return res.send(result);
-        }
+    connection.query(`select @rownum:=@rownum+1 as rownum, board_id, board_title, board_name, board_date from board, (select @rownum:=0) TMP order by rownum desc limit ${(req.params.currentPage - 1) * 10}, 10`, (err, result) => {
+
+        (err) ? res.send(err) : res.send(result);
     })
- });
- //---------------------------------------------------------
+});
+//---------------------------------------------------------
 
 module.exports = router;
